1eeeb94e56459fb7eb9c4def5163fa4a63a1bf12
Maxim <makfedorovtooo@gmail.com>
2022-12-01 19:36:16 +0300

    lection is done

---
+++ program_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+int Max(int ver1, int ver2, int ver3)
+{
+    int result = ver1;
+    if(result < ver2) { result = ver2; }
+    if(result < ver3) { result = ver3; }
+    return result;
+}
+int [] array = {1,3,4,5,6,7,8,9,2};
+int max = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4],array[5]), Max(array[6], array[7], array[8]));
+Console.WriteLine(max);
---
+++ program_4/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+void RandomNumber(int[] rand)
+{
+
+    int count = 0;
+    while(count < rand.Length)
+    {
+        rand[count] = new Random().Next(1,10);
+        count++;
+        
+    }
+};
+void IndexNumber(int[] index, int var)
+{
+    int count = 0;
+    Console.Write($"Число {var} находится под индексом: ");
+    while (count<index.Length)
+    {
+        if(var == index[count]) {Console.Write($"{count}, ");}
+        count++;
+    }
+}
+void PrintArray(int[] arr)
+{
+    int count = 0;
+    while(count< arr.Length)
+    {
+        Console.Write($"{arr[count]}, ");
+        count++;
+    }
+    Console.WriteLine();
+}
+int[] array = new int [10];
+int find = new Random().Next(1,10); 
+RandomNumber(array);
+PrintArray(array);
+IndexNumber(array,find);
